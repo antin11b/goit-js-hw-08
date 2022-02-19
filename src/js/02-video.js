271b3affb5f5891ae63ee0a1d9ae603478e2647e
Anton Bershak <anton.bershak@gmail.com>
2022-02-19 22:03:11 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,8 +5,17 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const onPlay = function ({ seconds }) {
-    localStorage.setItem("videoplayer-current-time", seconds);
+  localStorage.setItem('videoplayer-current-time', seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+getCurrentTime();
+
+function getCurrentTime() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+
+  if (currentTime) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  }
+}
