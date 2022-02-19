bceba32c7599fe2de43f6ca7a32cf23c408d6d93
Anton Bershak <anton.bershak@gmail.com>
2022-02-19 22:02:42 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,18 +5,19 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
-const GalleryItemMarkup = creatGalleryCardMarkup(galleryItems);
+const galleryItemMarkup = creatGalleryCardMarkup(galleryItems);
 
 function creatGalleryCardMarkup(galleryItems) {
-    return galleryItems
-        .map(({ original, preview, description }) => {
-            return `
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
             <a class="gallery__item" href="${original}">
             <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
         `;
-    }).join('');
-};
+    })
+    .join('');
+}
 
-galleryEl.insertAdjacentHTML('beforeend', GalleryItemMarkup);
-new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: 250 } );
+galleryEl.insertAdjacentHTML('beforeend', galleryItemMarkup);
+new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: 250 });
